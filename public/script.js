74209004283ce5946595762e96f4795895df9094
Joseph Kane <josephckane@gmail.com>
2016-05-26 14:00:53 -0500

    finish GET ajax, now it works

--- public/script.js
+++ public/script.js
@@ -1,10 +1,23 @@
 $(() => {
-	const API_URL = `https://crud-to-do-jk.firebaseio.com/`;
+	const API_URL = `https://crud-to-do-jk.firebaseio.com/task`;
+
+	function addDataToDOM (item, id) {
+		const row =
+		`<tr data-id="${id}">
+			<td>${item.task}</td>
+			<td>
+				<button class="btn btn-success complete">Complete</button>
+				<button class="btn btn-danger delete">Delete</button>
+			</td>
+		</tr>`
+
+		$('tbody').append(row);
+	}
 
 	$.get(`${API_URL}.json`)
 		.done((data) => (
 			Object.keys(data).forEach((id) => {
-				console.log(data[id], id, data)
+				addDataToDOM(data[id], id)
 			})
 		))
 
